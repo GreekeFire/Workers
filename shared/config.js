2f4eead178eabd2ae4bd4c71eb851949eb462273
GreekeFire <quekquinn@gmail.com>
2026-05-24 01:50:19 +0800

    Initial commit

---
+++ shared/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://tzwzmzabjmsocnxdtxqx.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_jvJXUrcqtFYroCF6tBNrsw_9hqAODjr';
---
+++ shared/utils.js
@@ -0,0 +1,6 @@
+function esc(str) {
+  if (!str) return '';
+  return String(str).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
+}
+
+function pad2(n) { return String(n).padStart(2, '0'); }
